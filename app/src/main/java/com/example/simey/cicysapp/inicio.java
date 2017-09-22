ea307a29e5589abc34c0b5314247219b731567c5
boskez23 <32045453+boskez23@users.noreply.github.com>
2017-09-22 08:56:12 -0500

    :-)

--- app/src/main/java/com/example/simey/cicysapp/inicio.java
+++ app/src/main/java/com/example/simey/cicysapp/inicio.java
@@ -1,20 +1,18 @@
 package com.example.simey.cicysapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
-    Button bt3;
+public class inicio extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_inicio);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,16 +24,6 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
                         .setAction("Action", null).show();
             }
         });
-
-        bt3 = (Button) findViewById(R.id.boton3);
-
-        bt3.setOnClickListener(this);
-
     }
 
-    @Override
-    public void onClick(View v) {
-        Intent intent = new Intent(v.getContext(), MainActivity.class);
-        startActivityForResult(intent,1);
-    }
 }
